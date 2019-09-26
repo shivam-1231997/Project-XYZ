b79996a2b94bd379a8f5bb53cef6234845a5fd18
shivam-1231997 <shivam.saxena19997@gmail.com>
2019-09-26 21:53:50 +0530

    animal.java

---
+++ animal.java
@@ -0,0 +1,20 @@
+class animal
+{
+animal()
+{
+System.out.println("this is animal construtor");
+}
+}
+class cow extends animal
+{
+cow()
+{
+System.out.println("this is a cow construtor");
+{
+class test
+{
+public static void main(String args[])
+{
+cow c = new cow();
+}
+}
